40e868d75ed3f4f984a3dab0286aab1c6c7a0600
uitgeteld <swendewit07@gmail.com>
2025-11-07 10:08:33 +0100

    Comment out Apply

--- src/main/java/nl/grapjeje/captureTheFlag/objects/CtfKit.java
+++ src/main/java/nl/grapjeje/captureTheFlag/objects/CtfKit.java
@@ -5,79 +5,80 @@ import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
 
 import static nl.grapjeje.captureTheFlag.enums.Kit.*;
 
 public class CtfKit {
-    public static void apply(CtfPlayer player) {
-        switch (player.getKit()){
-            case SCOUT -> {
-                giveItems(player,
-                        new ItemStack(Material.IRON_SWORD),
-                        new ItemStack(Material.BOW),
-                    new ItemStack(Material.ARROW, 16)
-                );
-                giveArmor(player,
-                        new ItemStack(Material.LEATHER_HELMET),
-                        new ItemStack(Material.LEATHER_CHESTPLATE),
-                        new ItemStack(Material.LEATHER_LEGGINGS),
-                        new ItemStack(Material.LEATHER_BOOTS)
-                );
-                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0));
-            }
-
-            case SOLDIER -> {
-                giveItems(player,
-                        new ItemStack(Material.IRON_SWORD),
-                        new ItemStack(Material.BOW),
-                        new ItemStack(Material.ARROW, 24)
-                );
-                giveArmor(player,
-                        new ItemStack(Material.IRON_HELMET),
-                        new ItemStack(Material.IRON_CHESTPLATE),
-                        new ItemStack(Material.IRON_LEGGINGS),
-                        new ItemStack(Material.IRON_BOOTS)
-                );
-            }
-
-            case HEAVY -> {
-                giveItem(player,
-                        new ItemStack(Material.IRON_AXE)
-                );
-                giveArmor(player,
-                        new ItemStack(Material.IRON_HELMET),
-                        new ItemStack(Material.IRON_CHESTPLATE),
-                        new ItemStack(Material.IRON_LEGGINGS),
-                        new ItemStack(Material.IRON_BOOTS)
-                );
-                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, Integer.MAX_VALUE, 0));
-                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, Integer.MAX_VALUE, 0));
-            }
-
-            case SNIPER -> {
-                giveItem(player,
-                        new ItemStack(Material.BOW),
-                        new ItemStack(Material.ARROW, 64)
-                        );
-                giveArmor(player,
-                        new ItemStack(Material.LEATHER_HELMET),
-                        new ItemStack(Material.LEATHER_CHESTPLATE),
-                        new ItemStack(Material.LEATHER_LEGGINGS),
-                        new ItemStack(Material.LEATHER_BOOTS)
-                );
-            }
-
-            case BESERKER -> {
-                giveItems(player,
-                        new ItemStack(Material.IRON_AXE)
-                );
-                giveArmor(player,
-                        new ItemStack(Material.CHAINMAIL_HELMET),
-                        new ItemStack(Material.CHAINMAIL_CHESTPLATE),
-                        new ItemStack(Material.CHAINMAIL_LEGGINGS),
-                        new ItemStack(Material.CHAINMAIL_BOOTS)
-                );
-            }
-        }
-    }
+//    public static void apply(@NotNull CtfPlayer player) {
+//        switch (player.getKit()){
+//            case SCOUT -> {
+//                giveItems(player,
+//                        new ItemStack(Material.IRON_SWORD),
+//                        new ItemStack(Material.BOW),
+//                    new ItemStack(Material.ARROW, 16)
+//                );
+//                giveArmor(player,
+//                        new ItemStack(Material.LEATHER_HELMET),
+//                        new ItemStack(Material.LEATHER_CHESTPLATE),
+//                        new ItemStack(Material.LEATHER_LEGGINGS),
+//                        new ItemStack(Material.LEATHER_BOOTS)
+//                );
+//                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0));
+//            }
+//
+//            case SOLDIER -> {
+//                giveItems(player,
+//                        new ItemStack(Material.IRON_SWORD),
+//                        new ItemStack(Material.BOW),
+//                        new ItemStack(Material.ARROW, 24)
+//                );
+//                giveArmor(player,
+//                        new ItemStack(Material.IRON_HELMET),
+//                        new ItemStack(Material.IRON_CHESTPLATE),
+//                        new ItemStack(Material.IRON_LEGGINGS),
+//                        new ItemStack(Material.IRON_BOOTS)
+//                );
+//            }
+//
+//            case HEAVY -> {
+//                giveItem(player,
+//                        new ItemStack(Material.IRON_AXE)
+//                );
+//                giveArmor(player,
+//                        new ItemStack(Material.IRON_HELMET),
+//                        new ItemStack(Material.IRON_CHESTPLATE),
+//                        new ItemStack(Material.IRON_LEGGINGS),
+//                        new ItemStack(Material.IRON_BOOTS)
+//                );
+//                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, Integer.MAX_VALUE, 0));
+//                player.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, Integer.MAX_VALUE, 0));
+//            }
+//
+//            case SNIPER -> {
+//                giveItem(player,
+//                        new ItemStack(Material.BOW),
+//                        new ItemStack(Material.ARROW, 64)
+//                        );
+//                giveArmor(player,
+//                        new ItemStack(Material.LEATHER_HELMET),
+//                        new ItemStack(Material.LEATHER_CHESTPLATE),
+//                        new ItemStack(Material.LEATHER_LEGGINGS),
+//                        new ItemStack(Material.LEATHER_BOOTS)
+//                );
+//            }
+//
+//            case BESERKER -> {
+//                giveItems(player,
+//                        new ItemStack(Material.IRON_AXE)
+//                );
+//                giveArmor(player,
+//                        new ItemStack(Material.CHAINMAIL_HELMET),
+//                        new ItemStack(Material.CHAINMAIL_CHESTPLATE),
+//                        new ItemStack(Material.CHAINMAIL_LEGGINGS),
+//                        new ItemStack(Material.CHAINMAIL_BOOTS)
+//                );
+//            }
+//        }
+//    }
 }
