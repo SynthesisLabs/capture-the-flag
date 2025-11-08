337a66ce1f852f99dea1a2735510323b20583a43
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-11-08 18:00:28 +0100

    Update + Remove Numbers

--- src/main/java/nl/grapjeje/captureTheFlag/objects/CtfScoreboard.java
+++ src/main/java/nl/grapjeje/captureTheFlag/objects/CtfScoreboard.java
@@ -1,11 +1,10 @@
 package nl.grapjeje.captureTheFlag.objects;
 
+import io.papermc.paper.scoreboard.numbers.NumberFormat;
 import lombok.Getter;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.Component;
 import nl.grapjeje.captureTheFlag.Main;
 import nl.grapjeje.captureTheFlag.enums.Team;
-import nl.grapjeje.captureTheFlag.models.PlayerModel;
 import nl.grapjeje.captureTheFlag.utils.MessageUtil;
 import nl.grapjeje.core.registry.AutoRegistry;
 import nl.grapjeje.core.registry.Registry;
@@ -24,6 +23,8 @@ public class CtfScoreboard {
     private static final String OBJECTIVE_NAME = "ctf_scoreboard";
 
     private final UUID uuid;
+    private Player player;
+    private CtfGame game;
 
     CtfScoreboard(UUID uuid) {
         this.uuid = uuid;
@@ -41,6 +42,9 @@ public class CtfScoreboard {
     }
 
     public void create(Player player, CtfGame game) {
+        this.player = player;
+        this.game = game;
+        
         ScoreboardManager manager = Bukkit.getScoreboardManager();
 
         String bluePoints = String.valueOf(game.getPoints().getOrDefault(Team.BLUE, 0));
@@ -48,8 +52,36 @@ public class CtfScoreboard {
 
         Scoreboard board = manager.getNewScoreboard();
 
+        Objective obj = board.registerNewObjective(OBJECTIVE_NAME, "dummy", Component.text("§6§lCAPTURE THE FLAG"));
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.numberFormat(NumberFormat.blank());
+
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<gray>")).setScore(8);
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<white>" + player.getName())).setScore(7);
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<gray>")).setScore(6);
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<white>Score:")).setScore(5);
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<green>" + bluePoints + "<white> - " + "<red>" + redPoints)).setScore(4);
+        obj.getScore(MessageUtil.filterMessageStringLegacy("<gray>")).setScore(3);
+
+        player.setScoreboard(board);
+    }
+
+    private void update() {
+        if (player == null || game == null) return;
+        
+        Scoreboard board = player.getScoreboard();
+
+        Objective oldObj = board.getObjective(OBJECTIVE_NAME);
+        if (oldObj != null) {
+            oldObj.unregister();
+        }
+        
+        String bluePoints = String.valueOf(game.getPoints().getOrDefault(Team.BLUE, 0));
+        String redPoints = String.valueOf(game.getPoints().getOrDefault(Team.RED, 0));
+
         Objective obj = board.registerNewObjective(OBJECTIVE_NAME, "dummy", "§6§lCAPTURE THE FLAG");
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.numberFormat(NumberFormat.blank());
 
         obj.getScore(MessageUtil.filterMessageStringLegacy("<gray>")).setScore(8);
         obj.getScore(MessageUtil.filterMessageStringLegacy("<white>" + player.getName())).setScore(7);
@@ -57,11 +89,6 @@ public class CtfScoreboard {
         obj.getScore(MessageUtil.filterMessageStringLegacy("<white>Score:")).setScore(5);
         obj.getScore(MessageUtil.filterMessageStringLegacy("<blue>" + bluePoints + "<white> - " + "<red>" + redPoints)).setScore(4);
         obj.getScore(MessageUtil.filterMessageStringLegacy("<gray>")).setScore(3);
-
-        player.setScoreboard(board);
-    }
-
-    private void update() {
     }
 
     public void tick() {
